348ca46691f68251f173b7928d57e940ae4ece5e
isaaclim <isaaclimjj@yahoo.com>
2021-06-10 23:50:12 +0800

    ready to add sub-specifier

--- source/ft_format_specifier.c
+++ source/ft_format_specifier.c
@@ -9,11 +9,6 @@ void	ft_format_specifier(t_print *info)
 		ft_output_string(info);
 	if (*info->format == 'd')
 		ft_output_int(info);
-	/*{
-		number = va_arg(info->args, int);
-		string = ft_itoa(number);
-		info->total_length += ft_putstr(string);
-		free (string);
-	}*/
+
 	info->format++;
 }
