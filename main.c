7f7ddba6b23e1996c40248f41ea53163a71f13e3
isaaclim <isaaclimjj@yahoo.com>
2021-06-10 12:18:09 +0800

    added %d

--- main.c
+++ main.c
@@ -4,10 +4,12 @@ int	main ()
 {
 	int ret;
 
-	ret = ft_printf("HBO\n");
+	ret = ft_printf("Just string\n");
 	printf("ret 1: %d\n", ret);
-	ret = ft_printf("Hello %s\n", "World123");
+	ret = ft_printf("Character: %c\n", 'A');
 	printf("ret 2: %d\n", ret);
-	ret = ft_printf("Hello %c\n", 'A');
+	ret = ft_printf("String: %s\n", "World123");
+	printf("ret 3: %d\n", ret);
+	ret = ft_printf("Integer: %d\n", 123);
 	printf("ret 3: %d\n", ret);
 }
--- source/ft_format_specifier.c
+++ source/ft_format_specifier.c
@@ -4,6 +4,7 @@ void	ft_format_specifier(t_print *info)
 {
 	char	letter;
 	char	*string;
+	int		number;
 
 	info->format++;
 	if (*info->format == 'c')
@@ -15,6 +16,13 @@ void	ft_format_specifier(t_print *info)
 	{
 		string = va_arg(info->args, char *);
 		info->total_length += ft_putstr(string);
+	}
+	if (*info->format == 'd')
+	{
+		number = va_arg(info->args, int);
+		string = ft_itoa(number);
+		info->total_length += ft_putstr(string);
+		free (string);
 	}
 	info->format++;
 }
