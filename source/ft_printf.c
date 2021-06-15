c4df31a0658ea917491f35ab632b4786699ddfd5
isaaclim <isaaclimjj@yahoo.com>
2021-06-15 23:33:14 +0800

    changed Makefile and location of ft_printf

---
+++ source/ft_printf.c
@@ -0,0 +1,28 @@
+#include "../includes/ft_printf.h"
+#include "../includes/ft_printf.h"
+
+int	ft_printf(const char *format, ...)
+{
+	t_print	*info;
+	int		len;
+
+	info = (t_print *)ft_calloc(1, sizeof(t_print));
+	if (!info)
+		return (-1);
+	va_start(info->args, format);
+	info->format = format;
+	while(*info->format)
+	{
+		while (*info->format != '%' && *info->format != '\0')
+		{
+			info->total_length += ft_putchar(*info->format);
+			info->format++;
+		}
+		if (*info->format == '%')
+			ft_format_flag(info);
+	}
+	va_end(info->args);
+	len = info->total_length;
+	free(info);
+	return (len);
+}
